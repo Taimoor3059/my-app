ebbaa9a7193dfa5ad6c7b097406d1bbb0a172fff
Taimoor Tahir <taimoorhtahir@gmail.com>
2019-07-30 18:04:20 -0500

    created Buttons component

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './components/Button';
+
+class Button extends Component {
+  render() {
+      return (
+          <div className='App'>
+          </div>
+         
+      );
+  }
+}
+export default Button;
