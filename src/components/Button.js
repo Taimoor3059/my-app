89914f2d60f10cc51d3988d080732d1d1d17e214
Taimoor Tahir <taimoorhtahir@gmail.com>
2019-07-30 22:17:05 -0500

    Styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import './components/Button';
+import './Button.css';
 
 class Button extends Component {
+
+  isOperator = val => {
+    return !isNaN(val) || val === '.' || val === '=';
+  };
+
   render() {
       return (
-          <div className='App'>
+          <div
+            className={`button ${this.isOperator
+            (this.props.children) ? '' : 'operator'}`}
+          >
+            {this.props.children}
           </div>
          
       );
